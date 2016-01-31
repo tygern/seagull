ad7ac93993b851170ecbf64292f37cdc700052da
Tyson Gern <tygern@gmail.com>
2016-01-31 14:56:52 +0000

    Add controller

--- components/birdfood-api/src/main/java/com/tygern/seagull/birdfoodapi/BirdfoodController.java
+++ components/birdfood-api/src/main/java/com/tygern/seagull/birdfoodapi/BirdfoodController.java
@@ -1,4 +1,12 @@
 package com.tygern.seagull.birdfoodapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BirdfoodController {
+    @RequestMapping("/birdfood")
+    public String birdfood() {
+        return "seed";
+    }
 }
